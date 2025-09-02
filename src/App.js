a544dfe5d53d6a24a5dffcd397c5e756ff530886
Marielly Sales <mariellysales01@icloud.com>
2025-09-02 13:26:19 -0300

    fix: rename enviroment variable CLARITY_PROJECT_ID

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ import ConsentModal from './components/ConsentModal';
 import Clarity from '@microsoft/clarity';
 
 function App() {
-    const projectId = process.env.CLARITY_PROJECT_ID;
+    const projectId = process.env.REACT_APP_CLARITY_PROJECT_ID;
 
     useEffect(() => {
         Clarity.init(projectId);
